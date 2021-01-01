6e5ef7e6e1d4e1719ba16a06ba08750fc9b2e349
Ilman Nafian <milmannafian04@gmail.com>
2021-01-02 00:09:30 +0700

    implement basic board generation

--- src/sweeper.rs
+++ src/sweeper.rs
@@ -1,8 +1,10 @@
 use std::fmt;
 
+use rand::Rng;
+
 pub const EASY_CONFIG: BoardConfig = BoardConfig {height: 9, width: 9, mine_count: 10};
-pub const MED_CONFIG: BoardConfig = BoardConfig {height: 16, width: 16, mine_count: 40};
-pub const HARD_CONFIG: BoardConfig = BoardConfig {height: 24, width: 24, mine_count: 99};
+// pub const MED_CONFIG: BoardConfig = BoardConfig {height: 16, width: 16, mine_count: 40};
+// pub const HARD_CONFIG: BoardConfig = BoardConfig {height: 24, width: 24, mine_count: 99};
 
 #[derive(Clone)]
 enum Cell {
@@ -44,7 +46,20 @@ pub struct BoardConfig {
 
 impl Board {
     pub fn new(config: BoardConfig) -> Self {
-        let board = vec![vec![Cell::Empty; config.width]; config.height];
+        let mut board = vec![vec![Cell::Empty; config.width]; config.height];
+        let mut placed_mine = 0;
+        let mut rng = rand::thread_rng();
+        while placed_mine < config.mine_count {
+            let i = rng.gen_range(0..config.height - 1);
+            let j = rng.gen_range(0..config.width - 1);
+            match board[i][j] {
+                Cell::Empty => {
+                    board[i][j] = Cell::Mine;
+                    placed_mine += 1;
+                }
+                _ => {}
+            }
+        }
         Board { config, cells: board }
     }
 }
