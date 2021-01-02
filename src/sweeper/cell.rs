08eb1b51b18b21e5b1c2942cd9517c145b638992
Ilman Nafian <milmannafian04@gmail.com>
2021-01-02 11:38:43 +0700

    generate board after first open

--- src/sweeper/cell.rs
+++ src/sweeper/cell.rs
@@ -4,16 +4,17 @@ use std::fmt;
 pub enum Cell {
     Mine,
     Flag,
-    Empty,
+    Free,
+    Uninitialized,
 }
 
 impl fmt::Debug for Cell {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Cell::Empty => f.write_str("  "),
+            Cell::Free => f.write_str("  "),
             Cell::Flag => f.write_str("! "),
             Cell::Mine => f.write_str("X "),
+            Cell::Uninitialized => f.write_str("/ "),
         }
     }
 }
-
--- src/sweeper/mod.rs
+++ src/sweeper/mod.rs
@@ -6,7 +6,11 @@ use cell::Cell;
 
 mod cell;
 
-pub const EASY_CONFIG: BoardConfig = BoardConfig {height: 9, width: 9, mine_count: 10};
+pub const EASY_CONFIG: BoardConfig = BoardConfig {
+    height: 9,
+    width: 9,
+    mine_count: 10,
+};
 // pub const MED_CONFIG: BoardConfig = BoardConfig {height: 16, width: 16, mine_count: 40};
 // pub const HARD_CONFIG: BoardConfig = BoardConfig {height: 24, width: 24, mine_count: 99};
 
@@ -17,8 +21,9 @@ pub struct BoardConfig {
 }
 
 pub struct Board {
-    config: BoardConfig,
     cells: Vec<Vec<Cell>>,
+    is_initiated: bool,
+    mine_count: usize,
 }
 
 impl fmt::Debug for Board {
@@ -33,20 +38,76 @@ impl fmt::Debug for Board {
 
 impl Board {
     pub fn new(config: BoardConfig) -> Self {
-        let mut board = vec![vec![Cell::Empty; config.width]; config.height];
+        Board {
+            cells: vec![vec![Cell::Uninitialized; config.width]; config.height],
+            is_initiated: false,
+            mine_count: config.mine_count,
+        }
+    }
+
+    fn initialize(&mut self, x: usize, y: usize) -> () {
+        // mark root and its neighbors as free
+        for i in 0..3 {
+            if x == 0 && i == 0 {
+                continue;
+            };
+            for j in 0..3 {
+                if y == 0 && j == 0 {
+                    continue;
+                };
+                match self.cells.get_mut(x + i - 1) {
+                    Some(row) => match row.get_mut(y + j - 1) {
+                        Some(cell) => {
+                            *cell = Cell::Free;
+                        }
+                        _ => (),
+                    },
+                    _ => (),
+                }
+            }
+        }
+        println!("{:?}", self);
+        // randomize mine placement
         let mut placed_mine = 0;
         let mut rng = rand::thread_rng();
-        while placed_mine < config.mine_count {
-            let i = rng.gen_range(0..config.height - 1);
-            let j = rng.gen_range(0..config.width - 1);
-            match board[i][j] {
-                Cell::Empty => {
-                    board[i][j] = Cell::Mine;
+        while placed_mine < self.mine_count {
+            let i = rng.gen_range(0..self.get_height());
+            let j = rng.gen_range(0..self.get_width());
+            match self.cells[i][j] {
+                Cell::Uninitialized => {
+                    self.cells[i][j] = Cell::Mine;
                     placed_mine += 1;
                 }
                 _ => {}
             }
         }
-        Board { config, cells: board }
+        println!("{:?}", self);
+        // fill the rest of uninitialized cells with free cell
+        for i in 0..self.get_height() {
+            for j in 0..self.get_width() {
+                match self.cells[i][j] {
+                    Cell::Uninitialized => {
+                        self.cells[i][j] = Cell::Free;
+                    }
+                    _ => (),
+                }
+            }
+        }
+        println!("{:?}", self);
+        self.is_initiated = true;
+    }
+
+    pub fn open(&mut self, x: usize, y: usize) -> () {
+        if !self.is_initiated {
+            self.initialize(x, y);
+        }
+    }
+
+    fn get_height(&self) -> usize {
+        self.cells.len()
+    }
+
+    fn get_width(&self) -> usize {
+        self.cells[0].len()
     }
 }
