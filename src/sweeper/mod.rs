0a0fcf904e980db94e25797ea57a447d03f7d315
Ilman Nafian <milmannafian04@gmail.com>
2021-01-03 11:50:10 +0700

    renaming vars that causes confusion

--- src/sweeper/mod.rs
+++ src/sweeper/mod.rs
@@ -47,9 +47,9 @@ impl Board {
 
     fn initialize(&mut self, x: usize, y: usize) -> () {
         // mark root and its neighbors as free
-        Board::traverse_neighbors(x, y, |i: usize, j: usize| {
-            if self.cell_is_within_range(x + i - 1, y + j - 1) {
-                self.cells[x + i - 1][y + j - 1].kind = CellKind::Free
+        Board::traverse_neighbors(x, y, |row: usize, col: usize| {
+            if self.cell_is_within_range(x + col - 1, y + row - 1) {
+                self.cells[x + col - 1][y + row - 1].kind = CellKind::Free
             }
         });
         // randomize mine placement
@@ -90,39 +90,39 @@ impl Board {
                 let neighbor_mines = self.count_neighbors_for_mine(x, y);
                 self.cells[x][y].mine_count = neighbor_mines;
                 if neighbor_mines == 0 {
-                    Board::traverse_neighbors(x, y, |i: usize, j: usize| {
-                        if i <= self.get_height() - 1 && j <= self.get_width() - 1 {
-                            self.open(x + i - 1, y + j - 1);
+                    Board::traverse_neighbors(x, y, |row: usize, col: usize| {
+                        if self.cell_is_within_range(x + col - 1, y + row - 1) {
+                            self.open(x + col - 1, y + row - 1);
                         }
                     })
                 }
-                Some(self.cells[x][y].kind.clone());
             }
+            Some(self.cells[x][y].kind.clone())
+        } else {
+            None
         }
-        None
     }
 
     fn count_neighbors_for_mine(&self, x: usize, y: usize) -> usize {
         let mut count = 0;
-        if self.cell_is_within_range(x + i - 1, y + j - 1) {
-            if self.cells[x][y]
-            Board::traverse_neighbors(x, y, |i: usize, j: usize| {
-                match self.cells[x + i - 1][y + j - 1].kind {
+        Board::traverse_neighbors(x, y, |row: usize, col: usize| {
+            if self.cell_is_within_range(x + col - 1, y + row - 1) {
+                match self.cells[x + col - 1][y + row - 1].kind {
                     CellKind::Mine => {
                         count += 1;
                     }
                     _ => (),
                 }
-            });
-        }
+            }
+        });
         count
     }
 
     fn cell_is_within_range(&self, x: usize, y: usize) -> bool {
-        if x >= self.get_height() - 1 {
+        if x >= self.get_width() - 1 {
             return false;
         };
-        if y >= self.get_width() - 1 {
+        if y >= self.get_height() - 1 {
             return false;
         };
         true
@@ -137,15 +137,15 @@ impl Board {
     }
 
     fn traverse_neighbors<F: FnMut(usize, usize)>(x: usize, y: usize, mut f: F) {
-        for i in 0..3 {
-            if x + i == 0 {
+        for row in 0..3 {
+            if y + row == 0 {
                 continue;
             }
-            for j in 0..3 {
-                if y + j == 0 {
+            for col in 0..3 {
+                if x + col == 0 {
                     continue;
                 }
-                f(i, j);
+                f(row, col);
             }
         }
     }
