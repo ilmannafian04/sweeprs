c0e07446a82aaf04ca22cc9a63b158c15792972f
ilmannafian04 <milmannafian04@gmail.com>
2021-07-22 12:23:26 +0700

    added open and flag traits

--- src/cell.rs
+++ src/cell.rs
@@ -22,6 +22,34 @@ pub struct Cell {
     pub mine_is_counted: bool,
 }
 
+pub trait Open {
+    fn open(&mut self) -> &CellKind;
+}
+
+impl Open for Cell {
+    fn open(&mut self) -> &CellKind {
+        if let CellState::Closed = self.state {
+            self.state = CellState::Open
+        }
+        &self.kind
+    }
+}
+
+pub trait Flag {
+    fn flag(&mut self) -> &CellState;
+}
+
+impl Flag for Cell {
+    fn flag(&mut self) -> &CellState {
+        match self.state {
+            CellState::Closed => self.state = CellState::Flagged,
+            CellState::Flagged => self.state = CellState::Closed,
+            _ => ()
+        }
+        &self.state
+    }
+}
+
 impl fmt::Debug for Cell {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self.state {
@@ -39,5 +67,70 @@ impl fmt::Debug for Cell {
                 CellKind::Uninitialized => f.write_str("/"),
             },
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn open_closed() {
+        let mut cell = Cell {
+            kind: CellKind::Free,
+            state: CellState::Closed,
+            mine_count: 0,
+            mine_is_counted: true,
+        };
+        cell.open();
+        assert!(matches!(cell.state, CellState::Open));
+    }
+
+    #[test]
+    fn open_flagged() {
+        let mut cell = Cell {
+            kind: CellKind::Free,
+            state: CellState::Flagged,
+            mine_count: 0,
+            mine_is_counted: true,
+        };
+        cell.open();
+        assert!(matches!(cell.state, CellState::Flagged));
+    }
+
+    #[test]
+    fn flag_closed() {
+        let mut cell = Cell {
+            kind: CellKind::Free,
+            state: CellState::Closed,
+            mine_count: 0,
+            mine_is_counted: true,
+        };
+        cell.flag();
+        assert!(matches!(cell.state, CellState::Flagged));
+    }
+
+    #[test]
+    fn flag_flagged() {
+        let mut cell = Cell {
+            kind: CellKind::Free,
+            state: CellState::Flagged,
+            mine_count: 0,
+            mine_is_counted: true,
+        };
+        cell.flag();
+        assert!(matches!(cell.state, CellState::Closed));
+    }
+
+    #[test]
+    fn flag_opened() {
+        let mut cell = Cell {
+            kind: CellKind::Free,
+            state: CellState::Open,
+            mine_count: 0,
+            mine_is_counted: true,
+        };
+        cell.flag();
+        assert!(matches!(cell.state, CellState::Open));
     }
 }
