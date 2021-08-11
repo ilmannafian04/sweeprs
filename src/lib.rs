83868b690ce47887085ac256cf1dcb6b9ea122f0
ilmannafian04 <milmannafian04@gmail.com>
2021-08-11 13:20:47 +0700

    reenabled exports

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,4 @@ mod board;
 pub mod cell;
 pub mod error;
 
-// pub use board::{Board, SweeperConfig, SweeperState};
-
-// pub const EASY_CONFIG: SweeperConfig = SweeperConfig {
-//     height: 9,
-//     width: 9,
-//     mine_count: 10,
-// };
-// pub const MED_CONFIG: SweeperConfig = SweeperConfig {
-//     height: 16,
-//     width: 16,
-//     mine_count: 40,
-// };
-// pub const HARD_CONFIG: SweeperConfig = SweeperConfig {
-//     height: 24,
-//     width: 24,
-//     mine_count: 99,
-// };
+pub use board::*;
